866211555611b93123263ef96887a8e2a5f1e5cd
Miki48RU <miki48ru@gmail.com>
2016-03-08 16:24:14 +0300

    Добавлен класс для расчета жеста смахивания

---
+++ app/src/main/java/ru/example/mike/ladlampeconomist/SwipeDetector.java
@@ -0,0 +1,54 @@
+package ru.example.mike.ladlampeconomist;
+
+import android.view.MotionEvent;
+
+/**
+ * Created by Mike on 06.03.2016.
+ */
+public class SwipeDetector {
+    private int swipe_distance;
+    private int swipe_velocity;
+    private static final int SWIPE_MIN_DISTANCE = 120;
+    private static final int SWIPE_THRESHOLD_VELOCITY = 200;
+
+    public SwipeDetector(int distance, int velocity){
+        super();
+        this.swipe_distance = distance;
+        this.swipe_velocity = velocity;
+    }
+
+    public SwipeDetector(){
+        super();
+        this.swipe_distance = SWIPE_MIN_DISTANCE;
+        this.swipe_velocity = SWIPE_THRESHOLD_VELOCITY;
+    }
+
+    public boolean isSwipeDown(MotionEvent e1, MotionEvent e2, float velocityY){
+        return isSwipe(e1.getY(), e2.getY(), velocityY);
+    }
+    public boolean isSwipeUp(MotionEvent e1, MotionEvent e2, float velocityY){
+        return isSwipe(e1.getY(), e2.getY(), velocityY);
+    }
+
+    public boolean isSwipeLeft(MotionEvent e1, MotionEvent e2, float velocityX){
+        return isSwipe(e1.getX(), e2.getX(), velocityX);
+    }
+
+    public boolean isSwipeRight(MotionEvent e1, MotionEvent e2, float velocityX){
+        return isSwipe(e1.getX(), e2.getX(), velocityX);
+    }
+
+    private boolean isSwipeDistance(float coordinateA, float coordinateB){
+        return (coordinateA - coordinateB) > this.swipe_distance;
+    }
+
+    private boolean isSwipeSpeed(float velocity){
+        return Math.abs(velocity) > this.swipe_velocity;
+    }
+
+    private boolean isSwipe(float coordinateA, float coordinateB, float velocity){
+        return isSwipeDistance(coordinateA, coordinateB)
+                && isSwipeSpeed(velocity);
+
+    }
+}
