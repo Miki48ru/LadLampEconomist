c8c03c9af669eec091a4d365ac5073069702ab61
Miki48RU <miki48ru@gmail.com>
2016-03-08 18:19:54 +0300

    добавлены ArrayList к спиннерам

--- app/src/main/java/ru/example/mike/ladlampeconomist/SpinnerFiller.java
+++ app/src/main/java/ru/example/mike/ladlampeconomist/SpinnerFiller.java
@@ -1,10 +1,38 @@
 package ru.example.mike.ladlampeconomist;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by Mike on 08.03.2016.
  */
 public class SpinnerFiller {
-    
 
+
+    private List<Integer> spinner1 = new ArrayList<>();
+    private List<Integer> spinner2 = new ArrayList<>();
+    private List<Integer> spinner3 = new ArrayList<>();
+
+    public List<Integer> getSpinner1() {
+        for (int i = 0; i < 50; i++) {
+            spinner1.add(i);
+        }
+
+        return spinner1;
+    }
+
+    public List<Integer> getSpinner2() {
+        for (int i = 0; i < 50; i++) {
+            spinner2.add(i);
+        }
+        return spinner2;
+    }
+
+    public List<Integer> getSpinner3() {
+        for (int i = 0; i < 50; i++) {
+            spinner3.add(i);
+        }
+        return spinner3;
+    }
 
 }
