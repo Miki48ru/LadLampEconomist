001a0f95b9c18eecdec61dad8f4e87b2b6588e91
Miki48RU <miki48ru@gmail.com>
2016-03-08 16:25:14 +0300

    Главная активность с приветствием

--- app/src/main/java/ru/example/mike/ladlampeconomist/MainActivity.java
+++ app/src/main/java/ru/example/mike/ladlampeconomist/MainActivity.java
@@ -1,13 +1,69 @@
 package ru.example.mike.ladlampeconomist;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.GestureDetector;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    private GestureDetector gestureDetector;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        gestureDetector = initGestureDetector();
+
+        View view = findViewById(R.id.main_activity);
+
+        view.setOnTouchListener(new View.OnTouchListener() {
+            public boolean onTouch(View v, MotionEvent event) {
+                return gestureDetector.onTouchEvent(event);
+            }
+        });
+
+        view.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View arg0) {
+            }
+        });
+    }
+
+    private GestureDetector initGestureDetector(){
+        return new GestureDetector(new GestureDetector.SimpleOnGestureListener() {
+
+            private SwipeDetector detector = new SwipeDetector();
+
+            public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX,
+                                   float velocityY) {
+                Intent intent = new Intent(MainActivity.this, RatesActivity.class);
+                try {
+                    if (detector.isSwipeDown(e1, e2, velocityY)) {
+                        return false;
+                    } else if (detector.isSwipeUp(e1, e2, velocityY)) {
+                        showToast("Проведите влево");
+                    }else if (detector.isSwipeLeft(e1, e2, velocityX)) {
+                        startActivity(intent);
+                    } else if (detector.isSwipeRight(e1, e2, velocityX)) {
+                        showToast("Проведите влево");
+                    }
+                } catch (Exception e) {} //for now, ignore
+                return false;
+            }
+
+            private void showToast(String phrase){
+                Toast.makeText(getApplicationContext(), phrase, Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+
+    public void onClickStart(View view) {
+        Intent intent = new Intent(MainActivity.this, RatesActivity.class);
+        startActivity(intent);
     }
 }
+
