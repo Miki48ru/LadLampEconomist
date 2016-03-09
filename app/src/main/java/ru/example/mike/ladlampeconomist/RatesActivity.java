a2ca765a843c23aa1217d3b82c667f77b4c6916c
Miki48RU <miki48ru@gmail.com>
2016-03-09 22:34:57 +0300

    добавлены адаптеры и заполнен список

--- app/src/main/java/ru/example/mike/ladlampeconomist/RatesActivity.java
+++ app/src/main/java/ru/example/mike/ladlampeconomist/RatesActivity.java
@@ -4,13 +4,40 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class RatesActivity extends AppCompatActivity {
+
+
+    private final int[] spinners = new int[]{R.id.spinnerOne,  R.id.spinnerTwo, R.id.spinnerHours};
+    private final List<List> spinnersData = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_rates);
+        fillData();
+        fillSpinners();
+    }
+
+    private void fillData(){
+        spinnersData.add(SpinnerFiller.getSpinnerOne());
+        spinnersData.add(SpinnerFiller.getSpinnerTwo());
+        spinnersData.add(SpinnerFiller.getSpinnerHours());
+    }
+
+
+    private void fillSpinners(){
+        for (int i = 0; i < spinners.length; i++) {
+            Spinner spinner = (Spinner)findViewById(spinners[i]);
+            ArrayAdapter<?> adapter = ArrayAdapter.createFromResource(this,  spinnersData.get(i), android.R.layout.simple_spinner_item);
+            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+            spinner.setAdapter(adapter);
+        }
     }
 
     public void onClickAgo(View view) {
