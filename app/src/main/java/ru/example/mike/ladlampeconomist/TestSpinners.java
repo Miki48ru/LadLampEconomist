df9118faf7144ace67e7da6763477cb5b77fa7eb
Miki48RU <miki48ru@gmail.com>
2016-03-09 22:35:30 +0300

    пробный класс

---
+++ app/src/main/java/ru/example/mike/ladlampeconomist/TestSpinners.java
@@ -0,0 +1,24 @@
+package ru.example.mike.ladlampeconomist;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by Mike on 09.03.2016.
+ */
+public class TestSpinners {
+
+
+    public List<Integer> getList() {
+        for (int i = 1; i <= 100; i++){
+            list.add(i);
+        }
+        return list;
+    }
+
+    private List<Integer> list = new ArrayList<>();
+
+    }
+
+
+
