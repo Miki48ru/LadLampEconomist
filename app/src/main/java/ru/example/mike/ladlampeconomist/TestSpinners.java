42271c3293a59950dd5fdfe6531a662503f76d1b
Miki48RU <miki48ru@gmail.com>
2016-03-13 16:29:25 +0300

    ничего

--- app/src/main/java/ru/example/mike/ladlampeconomist/TestSpinners.java
+++ app/src/main/java/ru/example/mike/ladlampeconomist/TestSpinners.java
@@ -1,24 +1,25 @@
 package ru.example.mike.ladlampeconomist;
 
+import android.app.Activity;
+
 import java.util.ArrayList;
 import java.util.List;
 
 /**
  * Created by Mike on 09.03.2016.
  */
-public class TestSpinners {
+public class TestSpinners extends Activity {
 
 
-    public List<Integer> getList() {
-        for (int i = 1; i <= 100; i++){
+    private static final List<Integer> list = new ArrayList<>();
+
+    static {
+        for (int i = 1; i <= 100; i++) {
             list.add(i);
         }
-        return list;
-    }
-
-    private List<Integer> list = new ArrayList<>();
 
     }
+}
 
 
 
