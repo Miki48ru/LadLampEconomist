b4e147a27e7ae60bb9787dd12918ad52f7c23adc
Miki48RU <miki48ru@gmail.com>
2016-03-13 16:29:40 +0300

    модифицируем под адапторы

--- app/src/main/java/ru/example/mike/ladlampeconomist/RatesActivity.java
+++ app/src/main/java/ru/example/mike/ladlampeconomist/RatesActivity.java
@@ -12,33 +12,33 @@ import java.util.List;
 
 public class RatesActivity extends AppCompatActivity {
 
+    private static List<Integer> spinnerOne = new ArrayList<>();
+    private static List<Integer> spinnerTwo = new ArrayList<>(); //TODO: перевести "копейку"
+    private static List<Integer> spinnerHours = new ArrayList<>();
+    static {
+        for (int i = 1; i <= 100; i++) {
+            spinnerOne.add(i);
+        }
+        for (int i = 1; i <= 50; i++) {
+            spinnerTwo.add(i);
+        }
+        for (int i = 1; i <= 24; i++) {
+            spinnerHours.add(i);
+        }
+    }
 
-    private final int[] spinners = new int[]{R.id.spinnerOne,  R.id.spinnerTwo, R.id.spinnerHours};
-    private final List<List> spinnersData = new ArrayList<>();
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_rates);
-        fillData();
-        fillSpinners();
-    }
 
-    private void fillData(){
-        spinnersData.add(SpinnerFiller.getSpinnerOne());
-        spinnersData.add(SpinnerFiller.getSpinnerTwo());
-        spinnersData.add(SpinnerFiller.getSpinnerHours());
     }
 
 
-    private void fillSpinners(){
-        for (int i = 0; i < spinners.length; i++) {
-            Spinner spinner = (Spinner)findViewById(spinners[i]);
-            ArrayAdapter<?> adapter = ArrayAdapter.createFromResource(this,  spinnersData.get(i), android.R.layout.simple_spinner_item);
-            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-            spinner.setAdapter(adapter);
-        }
-    }
+
+
 
     public void onClickAgo(View view) {
         Intent intent = new Intent(RatesActivity.this, MainActivity.class);
