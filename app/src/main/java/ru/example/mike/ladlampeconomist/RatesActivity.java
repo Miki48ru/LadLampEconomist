e9d83ca8499fd650d90d7592678a69dbf7282b70
Miki48RU <miki48ru@gmail.com>
2016-03-14 23:31:38 +0300

    добавил реализацию вывода результата при выборе последнего спинера, но не работает

--- app/src/main/java/ru/example/mike/ladlampeconomist/RatesActivity.java
+++ app/src/main/java/ru/example/mike/ladlampeconomist/RatesActivity.java
@@ -3,10 +3,14 @@ package ru.example.mike.ladlampeconomist;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
 import android.widget.SpinnerAdapter;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -16,6 +20,16 @@ public class RatesActivity extends AppCompatActivity {
     final private static List<Integer> spinnerOne = new ArrayList<>();
     final private static List<Integer> spinnerTwo = new ArrayList<>(); //TODO: перевести "копейку"
     final private static List<Integer> spinnerHours = new ArrayList<>();
+    final TextView textView = (TextView)findViewById(R.id.textView5);
+    Spinner spinnerRub = (Spinner) findViewById(R.id.spinnerOne);
+    Spinner spinnerKopeck = (Spinner) findViewById(R.id.spinnerTwo);
+    Spinner spinnerHour = (Spinner) findViewById(R.id.spinnerHours);
+    private int result;
+    private Integer selected1 = (Integer) spinnerRub. getSelectedItem();
+    private Integer selected2 = (Integer) spinnerKopeck. getSelectedItem();
+    private Integer selected3 = (Integer) spinnerHour. getSelectedItem();
+    final String LOG_TAG = "myLogs";
+
 
 
     static {
@@ -29,7 +43,7 @@ public class RatesActivity extends AppCompatActivity {
             spinnerHours.add(i);
         }
     }
-    
+
 
 
 
@@ -42,30 +56,49 @@ public class RatesActivity extends AppCompatActivity {
         adapterSpinnerTwo();
         adapterSpinnerHours();
 
+
     }
 
     public void adapterSpinnerOne(){
-        Spinner spinner = (Spinner) findViewById(R.id.spinnerOne);
+
         SpinnerAdapter spinnerAdapter = new ArrayAdapter<Integer>(this,
                 android.R.layout.simple_spinner_item, spinnerOne);
         ((ArrayAdapter<Integer>)spinnerAdapter).setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        spinner.setAdapter(spinnerAdapter);
+        spinnerRub.setAdapter(spinnerAdapter);
     }
 
     public void adapterSpinnerTwo(){
-        Spinner spinner = (Spinner) findViewById(R.id.spinnerTwo);
+
         SpinnerAdapter spinnerAdapter = new ArrayAdapter<Integer>(this,
                 android.R.layout.simple_spinner_item, spinnerTwo);
         ((ArrayAdapter<Integer>)spinnerAdapter).setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        spinner.setAdapter(spinnerAdapter);
+        spinnerKopeck.setAdapter(spinnerAdapter);
     }
-    public void adapterSpinnerHours(){
-        Spinner spinner = (Spinner) findViewById(R.id.spinnerHours);
+    public void adapterSpinnerHours() {
+
         SpinnerAdapter spinnerAdapter = new ArrayAdapter<Integer>(this,
                 android.R.layout.simple_spinner_item, spinnerHours);
-        ((ArrayAdapter<Integer>)spinnerAdapter).setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        spinner.setAdapter(spinnerAdapter);
+        ((ArrayAdapter<Integer>) spinnerAdapter).setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinnerHour.setAdapter(spinnerAdapter);
+        spinnerHour.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            public void onItemSelected(AdapterView<?> parent, View view,
+                                       int position, long id) {
+                int selected1 = Integer.valueOf(spinnerRub.getSelectedItem().toString());
+                int selected2 = Integer.valueOf(spinnerKopeck.getSelectedItem().toString());
+                int selected3 = Integer.valueOf(spinnerHour.getSelectedItem().toString());
+                Log.d("value ", "selected3 = " + spinnerHour.getSelectedItem().toString());
+                result = (selected1 + (selected2 / 100)) * selected3;
+                textView.setText(result);
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+                Toast.makeText(getApplicationContext(), "Нужно выбрать пункт", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
+
+
 
 
 
