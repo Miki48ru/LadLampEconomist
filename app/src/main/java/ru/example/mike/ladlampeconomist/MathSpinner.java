9007f1a08a577c406335f88fee7dfb915d4862ef
Miki48RU <miki48ru@gmail.com>
2016-03-15 18:20:57 +0300

    класс пока не используется

--- app/src/main/java/ru/example/mike/ladlampeconomist/MathSpinner.java
+++ app/src/main/java/ru/example/mike/ladlampeconomist/MathSpinner.java
@@ -8,18 +8,18 @@ import android.widget.Spinner;
  */
 public class MathSpinner extends Activity {
 
-   private Spinner spinner1 = (Spinner) findViewById(R.id.spinnerOne);
-   private Integer selected1 = spinner1. getSelectedItemPosition();
+//   private Spinner spinner1 = (Spinner) findViewById(R.id.spinnerOne);
+//   private Integer selected1 = spinner1. getSelectedItemPosition();
+//
+//   private Spinner spinner2 = (Spinner) findViewById(R.id.spinnerTwo);
+//   private Integer selected2 = spinner2. getSelectedItemPosition();
+//
+//   private Spinner spinner3 = (Spinner) findViewById(R.id.spinnerHours);
+//   private Integer selected3 = spinner3. getSelectedItemPosition();
 
-   private Spinner spinner2 = (Spinner) findViewById(R.id.spinnerTwo);
-   private Integer selected2 = spinner2. getSelectedItemPosition();
-
-   private Spinner spinner3 = (Spinner) findViewById(R.id.spinnerHours);
-   private Integer selected3 = spinner3. getSelectedItemPosition();
-
-    public int moneyTime(){
-        int result = (selected1+(selected2/100)) * selected3;
-        return result;
-    }
+//    public int moneyTime(){
+//        int result = (selected1+(selected2/100)) * selected3;
+//        return result;
+//    }
 
 }
