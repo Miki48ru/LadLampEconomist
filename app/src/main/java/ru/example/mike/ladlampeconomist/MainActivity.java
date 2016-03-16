d3fd86f25146bb65086a96371b1cd2ffe4e0f22e
Miki48RU <miki48ru@gmail.com>
2016-03-16 20:09:43 +0300

    swipe detecter

--- app/src/main/java/ru/example/mike/ladlampeconomist/MainActivity.java
+++ app/src/main/java/ru/example/mike/ladlampeconomist/MainActivity.java
@@ -1,6 +1,7 @@
 package ru.example.mike.ladlampeconomist;
 
 import android.content.Intent;
+import android.support.v4.view.GestureDetectorCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.GestureDetector;
@@ -9,56 +10,41 @@ import android.view.View;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-    private GestureDetector gestureDetector;
+    private GestureDetectorCompat mGestureDetectorCompat;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        gestureDetector = initGestureDetector();
-
-        View view = findViewById(R.id.main_activity);
-
-        view.setOnTouchListener(new View.OnTouchListener() {
-            public boolean onTouch(View v, MotionEvent event) {
-                return gestureDetector.onTouchEvent(event);
-            }
-        });
-
-        view.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View arg0) {
-            }
-        });
+        mGestureDetectorCompat = new GestureDetectorCompat(this, new MyGestureListener());
+    }
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        this.mGestureDetectorCompat.onTouchEvent(event);
+        return super.onTouchEvent(event);
     }
 
-    private GestureDetector initGestureDetector(){
-        return new GestureDetector(new GestureDetector.SimpleOnGestureListener() {
+    class MyGestureListener extends GestureDetector.SimpleOnGestureListener {
+        @Override
+        public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX, float velocityY) {
+            if (e2.getX() < e1.getX()) {
+                Toast.makeText(getBaseContext(),
+                        "Свайп влево",
+                        Toast.LENGTH_SHORT).show();
 
-            private SwipeDetector detector = new SwipeDetector();
-
-            public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX,
-                                   float velocityY) {
-                Intent intent = new Intent(MainActivity.this, RatesActivity.class);
-                try {
-                    if (detector.isSwipeDown(e1, e2, velocityY)) {
-                        return false;
-                    } else if (detector.isSwipeUp(e1, e2, velocityY)) {
-                        showToast("Проведите влево");
-                    }else if (detector.isSwipeLeft(e1, e2, velocityX)) {
-                        startActivity(intent);
-                    } else if (detector.isSwipeRight(e1, e2, velocityX)) {
-                        showToast("Проведите влево");
-                    }
-                } catch (Exception e) {} //for now, ignore
-                return false;
+                Intent intent = new Intent(
+                        MainActivity.this, RatesActivity.class);
+                startActivity(intent);
             }
-
-            private void showToast(String phrase){
-                Toast.makeText(getApplicationContext(), phrase, Toast.LENGTH_SHORT).show();
-            }
-        });
+            return true;
+        }
     }
+
+
+
+
 
 
     public void onClickStart(View view) {
