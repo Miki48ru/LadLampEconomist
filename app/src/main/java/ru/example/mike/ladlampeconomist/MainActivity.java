b394e001b28fc915defc62b6ddbe65a1f0c9171f
Miki48RU <miki48ru@gmail.com>
2016-03-22 16:43:10 +0300

    ничего нового

--- app/src/main/java/ru/example/mike/ladlampeconomist/MainActivity.java
+++ app/src/main/java/ru/example/mike/ladlampeconomist/MainActivity.java
@@ -1,16 +1,12 @@
 package ru.example.mike.ladlampeconomist;
 
 import android.content.Intent;
-import android.support.v4.view.GestureDetectorCompat;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.GestureDetector;
-import android.view.MotionEvent;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.widget.Toast;
+
 
 public class MainActivity extends AppCompatActivity {
-
 
 
     @Override
